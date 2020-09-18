d4c8dec4a49e668e66257a6319864b92ac7787bf
Ali Zand <alizand9224@gmail.com>
2020-09-17 22:25:27 -0700

    SB-7 notification (#22)
    * SB-8 Notification (#14)
    
    * SB-9 Added route for create notification. (#13)
    
    * SB-10 Create notification. add action and test (#17)
    
    Co-authored-by: Ali Zand <ali.zand9224@gmail.com>
    
    * SB-8 Remove useless Rubocop
    
    Co-authored-by: Damien Levitt <50222822+damienlevitt@users.noreply.github.com>
    Co-authored-by: Ali Zand <ali.zand9224@gmail.com>
    
    * SB-13 Add action and route to get notifications for the user. Add test
    
    * SB-13 actually throw an error when user is not signed in
    
    * SB-13 Add action and route to get notifications for the user. Add test (#18)
    
    Co-authored-by: Ali Zand <ali.zand9224@gmail.com>
    
    * SB-14 Update User to user_id (#19)
    
    Co-authored-by: Ali Zand <ali.zand9224@gmail.com>
    
    * SB-11 Create show action and route (#20)
    
    Co-authored-by: Ali Zand <ali.zand9224@gmail.com>
    
    * SB-15 Add Route for update, create method to check ownsership (#21)
    
    Co-authored-by: Ali Zand <ali.zand9224@gmail.com>
    
    Co-authored-by: Damien Levitt <50222822+damienlevitt@users.noreply.github.com>
    Co-authored-by: Ali Zand <ali.zand9224@gmail.com>

---
+++ app/controllers/notifications_controller.rb
@@ -0,0 +1,64 @@
+# frozen_string_literal: true
+
+class NotificationsController < ApplicationController
+  before_action :authenticate_user!, only: %i[create index]
+
+  def index
+    raise SecurityError unless current_user.present?
+
+    notifications = Notification.where(user_id: current_user.id)
+
+    render json: { notifications: notifications }, status: :ok
+  rescue SecurityError
+    render json: { error: 'User needs to be logged in' }, status: :unauthorized
+  end
+
+  def create
+    notification = Notification.new(notification_params)
+    notification.user_id = current_user.id
+
+    notification.save!
+
+    render json: { notification: notification }, status: :ok
+  end
+
+  def show
+    if user_signed_in?
+      notification = Notification.find(params[:id])
+
+      if !notification.belongs_to_user(current_user)
+        render json: { error: 'You do not have correct permissions' }, status: :unauthorized
+      else
+        render json: {
+          notification: {
+            name: notification.name,
+            description: notification.description,
+          },
+        }, status: :ok
+      end
+    else
+      render json: { error: 'User needs to be logged in' }, status: :unauthorized
+    end
+  end
+
+  def update
+    if user_signed_in?
+      notification = Notification.find(params[:id])
+
+      if notification.belongs_to_user(current_user)
+        notification.update(notification_params)
+        notification.save!
+
+        render json: { success: 'ok' }, status: :ok
+      else
+        render json: { error: 'You do not have correct permissions' }, status: :unauthorized
+      end
+    end
+  end
+
+  private
+
+  def notification_params
+    params.permit(:name, :description)
+  end
+end
---
+++ spec/controllers/notifications_controller_spec.rb
@@ -0,0 +1,89 @@
+# frozen_string_literal: true
+
+RSpec.describe NotificationsController do
+  describe 'POST create' do
+    describe 'user logged in' do
+      let(:user) { create(:user) }
+      let(:params) do
+        {
+          name: 'Battery Low',
+          description: 'Notify the user when the battery gets below 20%',
+        }
+      end
+      let(:notification) { JSON.parse(response.body)['notification'] }
+
+      before do
+        sign_in user
+      end
+
+      it 'creates returns a :ok status' do
+        post :create, params: params
+
+        expect(response.status).to eq(200)
+      end
+
+      it 'response with the notification' do
+        post :create, params: params
+
+        expect(notification).to be_present
+      end
+
+      it 'has an id (is saved)' do
+        post :create, params: params
+
+        expect(notification['id']).to be_kind_of(Integer)
+      end
+
+      it 'has the user id of the current user' do
+        post :create, params: params
+
+        expect(notification['user_id']).to eq(user.id)
+      end
+
+      it 'has the name saved correctly' do
+        post :create, params: params
+
+        expect(notification['name']).to eq(params[:name])
+      end
+
+      it 'has the description saved correctly' do
+        post :create, params: params
+
+        expect(notification['description']).to eq(params[:description])
+      end
+    end
+  end
+
+  describe 'GET index' do
+    let(:user) { create(:user) }
+    let!(:notification) { create(:notification, user_id: user.id) }
+
+    context 'user logged in' do
+      let(:res) { JSON.parse(response.body)['notifications'].to_json }
+
+      before do
+        sign_in user
+      end
+
+      it 'gets the notification from the database' do
+        get :index
+
+        expect(res).to eq([notification].to_json)
+      end
+    end
+
+    context 'user is not logged in' do
+      before do
+        allow_any_instance_of(NotificationsController)
+          .to receive(:authenticate_user!)
+          .and_return(nil)
+      end
+
+      it 'has unauthorized status' do
+        get :index
+
+        expect(response.status).to eq(401)
+      end
+    end
+  end
+end
