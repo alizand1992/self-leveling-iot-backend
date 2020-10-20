8093737b5481003f02e48b50f02c71d751075a9f
Ali Zand <ali.zand9224@gmail.com>
2020-10-19 17:02:34 -0700

    SB-27 Create the method to pull all devices, add test

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,3 +1,32 @@
+# frozen_string_literal: true
+
+require 'net/http'
+
 class Device < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
+
+  SYNC_ERROR = 'There was an error syncing devices'
+
+  def self.sync_with_aws
+    uri = URI.parse('https://2v0ejsryj6.execute-api.us-west-1.amazonaws.com/devices/all123')
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Get.new(uri.path)
+    response = http.request(request)
+    code = JSON.parse(response.code)
+    raise Exception.new('There was an error syncing devices') if code != 200
+
+    items = JSON.parse(response.body)
+
+    ids = items.map { |item| item['id']['S'] }
+
+    already_existing = self.where(aws_device_id: ids).pluck(:aws_device_id)
+    new_ids = ids - already_existing
+
+    devices = new_ids.map { |id| { aws_device_id: id } }
+
+    self.create!(devices)
+  end
 end
--- spec/models/device_spec.rb
+++ spec/models/device_spec.rb
@@ -1,5 +1,99 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Device, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#sync_with_aws' do
+    subject { described_class.sync_with_aws }
+
+    context 'fails to make a connection' do
+      class ErrorRes
+        attr_reader :code
+
+        def initialize
+          @code = '403'
+        end
+      end
+
+      let(:error_res) { ErrorRes.new }
+
+      before do
+        Net::HTTP.any_instance.stub(:request).and_return(error_res)
+      end
+
+      it 'raises an exception' do
+        expect { subject }.to raise_error(Exception)
+      end
+
+      it 'has the right message' do
+        expect { subject }.to raise_error(Device::SYNC_ERROR)
+      end
+    end
+
+    context 'successfully makes a connection' do
+      let(:expected_ids) do
+        %w[
+          e750ec0f-00a8-44d5-8881-c4edd52fb6ab
+          e750ec0f-00a8-44d5-8881-c4edd52fb6ac
+        ]
+      end
+      class SuccessRes
+        attr_reader :code, :body
+
+        def initialize
+          @code = '200'
+          @body = "[{\"healthy\":{\"BOOL\":true},\"password\":{\"S\":\"03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4\"},\"id\":{\"S\":\"e750ec0f-00a8-44d5-8881-c4edd52fb6ab\"},\"level\":{\"BOOL\":true},\"battery\":{\"N\":\"80.5\"}}, {\"healthy\":{\"BOOL\":true},\"password\":{\"S\":\"03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4\"},\"id\":{\"S\":\"e750ec0f-00a8-44d5-8881-c4edd52fb6ac\"},\"level\":{\"BOOL\":true},\"battery\":{\"N\":\"80.5\"}}]"
+        end
+      end
+
+      let(:success_res) { SuccessRes.new }
+
+      before do
+        Net::HTTP.any_instance.stub(:request).and_return(success_res)
+      end
+
+      context 'no data in the database yet' do
+        before do
+          subject
+        end
+
+        it 'has two entries in DB' do
+          expect(described_class.count).to eq 2
+        end
+
+        it 'has the expected ids' do
+          described_class.all.pluck(:aws_device_id).each do |id|
+            expect(expected_ids).to include id
+          end
+        end
+      end
+
+      context 'there is already an id in the DB' do
+        let(:new_id) { 'e750ec0f-00a8-44d5-8881-c4edd52fb6ac' }
+        let(:old_id) { 'e750ec0f-00a8-44d5-8881-c4edd52fb6ab' }
+
+        before do
+          create(:unregistered_device, aws_device_id: old_id)
+        end
+
+        it 'only adds one' do
+          expect(Device.count).to eq 1
+          subject
+          expect(Device.count).to eq 2
+        end
+
+        it 'adds the right one' do
+          expect(Device.where(aws_device_id: new_id).size).to eq 0
+          subject
+          expect(Device.where(aws_device_id: new_id).size).to eq 1
+        end
+
+        it 'does not change the old one' do
+          old_device = Device.where(aws_device_id: old_id).first
+          subject
+          expect(Device.where(aws_device_id: old_id).first).to eq old_device
+        end
+      end
+    end
+  end
 end
