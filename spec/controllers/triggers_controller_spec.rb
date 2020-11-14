3b8410ec1e62a365ce63fa17f772ad3efc00efe8
Ali Zand <ali.zand9224@gmail.com>
2020-11-13 22:05:27 -0800

    SB-35 Add test for update

--- spec/controllers/triggers_controller_spec.rb
+++ spec/controllers/triggers_controller_spec.rb
@@ -1,6 +1,70 @@
 # frozen_string_literal: true
 
 RSpec.describe TriggersController do
+  describe 'GET index' do
+    context 'user logged in' do
+      let(:user) { create(:user) }
+      let(:params) {{ notification_id: notification.id }}
+
+      before do
+        sign_in user
+      end
+
+      context 'notification belongs to this user' do
+        let(:notification) { create(:notification, user: user) }
+        let(:triggers) { JSON.parse(response.body)['triggers'] }
+
+        context 'no triggers are saved' do
+          it 'returns empty triggers' do
+            get :index, params: params
+
+            expect(triggers).to eq([])
+          end
+        end
+
+        context 'triggers dont belong to notification' do
+          let(:another_notification) { create(:notification, user: user) }
+          let!(:trigger) { create(:trigger, notification: another_notification) }
+
+          it 'returns empty triggers' do
+            get :index, params: params
+
+            expect(triggers).to eq([])
+          end
+        end
+
+        context 'triggers belong to notification' do
+          let!(:trigger) { create(:trigger, notification: notification) }
+
+          it 'returns empty triggers' do
+            get :index, params: params
+
+            expect(triggers).not_to be_blank
+          end
+        end
+      end
+
+      context 'notification does NOT belong to this user' do
+        let(:another_user) { create(:user) }
+        let(:notification) { create(:notification, user: another_user) }
+        let(:triggers) { JSON.parse(response.body)['triggers'] }
+        let(:error) { JSON.parse(response.body)['error'] }
+
+        it 'creates returns a :unathorized status' do
+          post :create, params: params
+
+          expect(response.status).to eq(401)
+        end
+
+        it 'response with the success' do
+          post :create, params: params
+
+          expect(error).to eq(Trigger::DOES_NOT_OWN_NOTIFICATION_MSG)
+        end
+      end
+    end
+  end
+
   describe 'POST create' do
     context 'user logged in' do
       let(:user) { create(:user) }
@@ -69,67 +133,36 @@ RSpec.describe TriggersController do
     end
   end
 
-  describe 'GET index' do
-    context 'user logged in' do
-      let(:user) { create(:user) }
-      let(:params) {{ notification_id: notification.id }}
+  describe 'PUT/PATCH update' do
+    let(:user) { create(:user) }
+    let(:params) do
+      {
+        id: trigger.id,
+        aws_column: 'battery',
+        relationship: 'less than',
+        trigger_type: 'float',
+        value: '22.0',
+        notification_id: notification.id
+      }
+    end
 
-      before do
-        sign_in user
-      end
+    let(:notification) { create(:notification, user: user) }
+    let(:trigger) { create(:trigger, notification: notification) }
 
-      context 'notification belongs to this user' do
-        let(:notification) { create(:notification, user: user) }
-        let(:triggers) { JSON.parse(response.body)['triggers'] }
+    before do
+      sign_in user
+    end
 
-        context 'no triggers are saved' do
-          it 'returns empty triggers' do
-            get :index, params: params
+    it 'responses with a 200' do
+      put :update, params: params
 
-            expect(triggers).to eq([])
-          end
-        end
+      expect(response.status).to eq(200)
+    end
 
-        context 'triggers dont belong to notification' do
-          let(:another_notification) { create(:notification, user: user) }
-          let!(:trigger) { create(:trigger, notification: another_notification) }
+    it 'updates the trigger' do
+      put :update, params: params
 
-          it 'returns empty triggers' do
-            get :index, params: params
-
-            expect(triggers).to eq([])
-          end
-        end
-
-        context 'triggers belong to notification' do
-          let!(:trigger) { create(:trigger, notification: notification) }
-
-          it 'returns empty triggers' do
-            get :index, params: params
-
-            expect(triggers).not_to be_blank
-          end
-        end
-      end
-
-      context 'notification does NOT belong to this user' do
-        let(:another_user) { create(:user) }
-        let(:notification) { create(:notification, user: another_user) }
-        let(:triggers) { JSON.parse(response.body)['triggers'] }
-        let(:error) { JSON.parse(response.body)['error'] }
-
-        it 'creates returns a :unathorized status' do
-          post :create, params: params
-
-          expect(response.status).to eq(401)
-        end
-
-        it 'response with the success' do
-          post :create, params: params
-
-          expect(error).to eq(Trigger::DOES_NOT_OWN_NOTIFICATION_MSG)
-        end
-      end
+      expect(Trigger.find(trigger.id).value).to eq('22.0')
     end
   end
 
